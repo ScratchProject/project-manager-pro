3c15f4e3a951b3c196a050e967e9837129081b9c
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-04-03 16:45:57 -0700

    began work on update form

--- client/components/feature/InfoBtn.jsx
+++ client/components/feature/InfoBtn.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+//create a function for onClick.
+//onClick should create a popup with all of the projects relevant information
+//
+
+
+
 class InfoBtn extends Component {
   render() {
     return (
      <div className="info-btn">
-       <button type="button" className="update-btn">Update Info</button>
+       <button type="button" className="update-btn" onClick={() => {this.props.showUpdateForm(this.props.index)}}>Update Info</button>
      </div>
    );
   }
