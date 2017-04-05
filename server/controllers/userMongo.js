1cdcfa90820f00ee0be1f101e89f4388cc0fd7a7
Justin Pierson <justinpierson@Justins-MacBook-Pro.local>
2017-04-05 14:34:07 -0700

    test file complete

--- server/controllers/userMongo.js
+++ server/controllers/userMongo.js
@@ -3,32 +3,34 @@ const User = require('../models/userModel');
 
 const userController = {
 
-  createUser(req, res) {
-    User.create({
-      username: req.body.username,
-      password: req.body.password,
-    }, (err, result) => {
-      if (err) console.log(err);
-      res.send(result);
-    })
-  },
-
   verifyUser(req, res) {
     User.findOne({ username: req.body.username }, (err, result) => {
       console.log('REQ BODY', req.body);
       console.log('RESULT OF SERVER FINDING USERNAME', result);
       if (result === null) {
-        return res.send(false);
+        return res.status(401).send('User does not exist');
       } else {
         if (result.password === req.body.password) {
-          return res.send(true);
+          return res.status(200).send(true);
         } else {
-          return res.send(false);
+          return res.status(401).send('User does not exist');
         }
       }
     })
-  }
+  },
 
+  createUser(req, res) {
+    User.findOne({ username: req.body.username }, (err, result) => {
+      if (result === null) {
+        User.create({
+          username: req.body.username,
+          password: req.body.password,
+        }, res.status(200).send(true))
+      } else if (err || result !== null) {
+        return res.status(400).send(err);
+      }
+    })
+  }
 }
 
 module.exports = userController;
--- test/test.js
+++ test/test.js
@@ -1,51 +1,38 @@
-const mocha = require('mocha');
-const expect = require('chai').expect;
-const request = require('request');
-const supertest = require('supertest');
+const request = require('supertest')('http://localhost:8000');
 const fs = require('fs');
 const path = require('path');
 const host = 'http://localhost:8000';
 const port = process.env.port || 8000;
 
-
-//Example - Passes Test
-// describe('db unit tests', () => {
-// 	describe('GET', () => {
-// 		it('returns a status 200', () => {
-// 			request.get('http://localhost:8000/createUser', (err, res) => {
-// 				expect(res.statusCode).toEqual(200);
-// 			});
-// 		});
-// 	});
-// });
-
 describe('db unit tests', () => {
 	//tests userController.verify method
 	describe('POST /verifyUser', () => {
-		it('returns status code 200 if username/pw match user in db', () => {
+		it('returns status code 200 if username/pw match user in db', (done) => {
 			const user = { username: 'Bob', password: '1234' }
-			request.post('http://localhost:8000/verifyUser')
+			request.post('/verifyUser')
 				.send(user)
 				.expect(200, done)
 		});
-		it('returns 401 if username/pw do not exist in database', () => {
-			const user = { username: 'Morgan', password: 'loser' }
-			request.post('http://localhost:8000/verifyUser')
+		it('returns 401 if username/pw do not exist in database', (done) => {
+			//change username and password for every new test
+			const user = { username: 'user2', password: '123' }
+			request.post('/verifyUser')
 				.send(user)
 				.expect(401, done)
 		});
 	});
-	describe('POST', () => {
+	describe('POST /createUser', () => {
 		//tests userController.create method
-		it('return 200 if new user has been added to the db', () => {
-			const user = { username: 'Cory', password: '4321' }
-			request.post('http://localhost:8000/createUser')
+		it('return 200 if new user has been added to the db', (done) => {
+			//change username and password for every new test
+			const user = { username: 'user3', password: '123' }
+			request.post('/createUser')
 				.send(user)
 				.expect(200, done)
 		});
-		it('return 400 if user already exists', () => {
-			const user = { username: 'Chris', password: 'ilovetesting' }
-			request.post('http://localhost:8000/createUser')
+		it('return 400 if user already exists', (done) => {
+			const user = { username: 'Bob', password: '1234' }
+			request.post('/createUser')
 				.send(user)
 				.expect(400, done)
 		});
@@ -62,9 +49,9 @@ describe('db unit tests', () => {
 	// 	});
 	// });
 	//tests userController.destroy method
-	describe('GET', () => {
-		it('checks if a user has been deleted from db', () => {
-			request.get('http://localhost:8000/api/user/:username')
-		});
-	});
+	// describe('GET', () => {
+	// 	it('checks if a user has been deleted from db', () => {
+	// 		request.get('http://localhost:8000/api/user/:username')
+	// 	});
+	// });
 });
